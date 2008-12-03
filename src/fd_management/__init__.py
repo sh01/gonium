81699f060e9613a77aae3716a590a14de2ffb25e
Sebastian Hagen <sebastian_hagen@memespace.net>
2008-12-03 18:25:25 +0100

    fd_management: rename attributes and methods to clarify non-publicness

--- src/fd_management/__init__.py
+++ src/fd_management/__init__.py
@@ -41,76 +41,76 @@ import time
 class FDMError(Exception):
    pass
 
-__logger = logging.getLogger('gonium.fd_management')
-__log = __logger.log
+_logger = logging.getLogger('gonium.fd_management')
+_log = _logger.log
 
 class AsyncDataStream:
    """Class for asynchronously accessing streams of bytes of any kind."""
    def __init__(self, ed, inbufsize_start: int=1024, inbufsize_max:int=0, size_need:int=0):
-      self.f
-      self.__size_need = size_need # how many bytes to read before calling input_process2()
+      self._f
+      self.size_need = size_need # how many bytes to read before calling input_process2()
       assert(inbufsize_start > 0)
-      self.inbuf = bytearray(inbufsize_start)
-      self.inbuf_size = inbufsize_start
-      self.inbuf_size_max = inbufsize_max
-      self.inbuf_index = 0
+      self._inbuf = bytearray(inbufsize_start)
+      self._inbuf_size = inbufsize_start
+      self._inbuf_size_max = inbufsize_max
+      self._index_in = 0
    
-   def inbuf_resize(self, new_size=None):
+   def _inbuf_resize(self, new_size=None):
       """Increase size of self.inbuf without discarding data"""
-      if (self.inbuf_size >= self.inbuf_size_max >= 0):
-         __logger.log(30, 'Closing {0} because buffer limit {0.inbuf_size_max} has been hit.'.format(self))
+      if (self._inbuf_size >= self._inbuf_size_max >= 0):
+         _log(30, 'Closing {0} because buffer limit {0.inbuf_size_max} has been hit.'.format(self))
          self.close()
       if (new_size is None):
-         new_size = self.inbuf_size * 2
-      new_size = min(new_size, self.inbuf_size_max)
-      self.inbuf_size = new_size
+         new_size = self._inbuf_size * 2
+      new_size = min(new_size, self._inbuf_size_max)
+      self._inbuf_size = new_size
       inbuf_new = bytearray(new_size)
-      inbuf_new[:self.index_in] = self.inbuf[:self.index_in]
-      self.inbuf = inbuf_new
+      inbuf_new[:self._index_in] = self._inbuf[:self._index_in]
+      self._inbuf = inbuf_new
    
-   def data_read(self):
+   def _data_read(self):
       """Read and buffer input from wrapped file-like object"""
-      br = self.f.readinto(memoryview(self.f)[self.inbuf_index:])
+      br = self._f.readinto(memoryview(self._inbuf)[self._index_in:])
       if (br == 0):
          self.close()
-      self.index_in += br
+      self._index_in += br
    
-   def process_input0(self):
+   def _process_input0(self):
       """Input processing stage 0: read and buffer bytes"""
-      self.data_read()
-      if (self.index_in >= self.size_need):
-         self.process_input1(self)
-      if (self.index_in >= self.inbuf_size):
-         self.inbuf_resize()
+      self._data_read()
+      if (self._index_in >= self.size_need):
+         self._process_input1(self)
+      if (self._index_in >= self._inbuf_size):
+         self._inbuf_resize()
 
-   def process_input1(self):
+   def _process_input1(self):
       """Override in subclass to insert more handlers"""
       self.process_input()
 
    def inbuf_data_discard(self, bytes:int):
       """Discard <bytes> of in-buffered data."""
-      inbuf_new = bytearray(self.inbuf_size)
-      inbuf_new[bytes:self.inbuf_size] = self.inbuf[bytes:self.inbuf_size]
-      self.inbuf = inbuf_new
+      inbuf_new = bytearray(self._inbuf_size)
+      inbuf_new[bytes:self._inbuf_size] = self._inbuf[bytes:self._inbuf_size]
+      self._inbuf = inbuf_new
 
 
 def AsyncLineStream(AsyncDataStream):
    """Class for asynchronously accessing line-based bytestreams"""
    def __init__(self, ed, lineseps:collections.Set=('n',), **kwargs):
       super().__init__(ed, **kwargs)
-      self.inbuf_index_l = 0
-      self.ls = lineseps
-      self.ls_maxlen = max([len(s) for s in lineseps])
+      self._inbuf_index_l = 0
+      self._ls = lineseps
+      self._ls_maxlen = max([len(s) for s in lineseps])
       
    def process_input1(self):
       """Input processing stage 1: split data into lines"""
       # Make sure we don't skip over seperators partially read earlier
-      index_l = min(0, self.inbuf_index_l-self.ls_maxlen+1)
+      index_l = min(0, self._inbuf_index_l-self._ls_maxlen+1)
       line_start = 0
       while (True):
          line_end = None
-         for sep in self.ls:
-            i = self.inbuf(sep, index_l, self.inbuf_size)
+         for sep in self._ls:
+            i = self._inbuf(sep, index_l, self._inbuf_size)
             if (i == -1):
                continue
             if (line_end is None):
@@ -120,7 +120,7 @@ def AsyncLineStream(AsyncDataStream):
          
          if (line_end is None):
             break
-         self.process_input2(self,memoryview(self.inbuf)[line_start:line_end])
+         self._process_input2(self,memoryview(self._inbuf)[line_start:line_end])
          line_start = index_l = line_end
       if (line_start):
          self.inbuf_data_discard(line_start)
